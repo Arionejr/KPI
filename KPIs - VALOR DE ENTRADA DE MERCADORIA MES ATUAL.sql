b42462e9e35708660279488afa9b0ba8d2880226
Arionejr <arionejr@gmail.com>
2019-11-19 11:42:29 -0300

    ajuste devolucao rotina 158

--- KPIs - VALOR DE ENTRADA DE MERCADORIA MES ATUAL.sql
+++ KPIs - VALOR DE ENTRADA DE MERCADORIA MES ATUAL.sql
@@ -1,98 +1,98 @@
 --AUTOR CONSULTOR TOTVS ARIONE.JUNIOR@TOTVS.COM.BR
 --NOME RELATORIO: VALOR DE ENTRADA DE MERCADORIA NO MÊS ATUAL
---ROTINA 218
+--ROTINA 218 VALIDANDO DEVOLUÇÃO DE FORNECEDOR ROTINA 158
 --ERP WINTHOR
 --TIPO DO GRÁFICO: ABSOLUTO
 
-SELECT SUM(TABELA.VLTOTAL) VLTOTAL FROM ( 
-SELECT PCMOV.CODPROD                               
-     , PCMOV.DTMOV                                 
-     , PCMOV.NUMSEQ                                
-     , PCPRODUT.DESCRICAO                          
-     , NVL(PCMOV.PERCDESC, 0)       PERCDESC       
-     , NVL(PCMOV.VLDESCONTO, 0)     VLDESCONTO     
-     , NVL(PCMOV.PERCSUFRAMA, 0)    PERCSUFRAMA    
-     , NVL(PCMOV.VLSUFRAMA, 0)      VLSUFRAMA      
-     , NVL(PCMOV.PERCFRETE, 0)      PERCFRETECIF   
-     , NVL(PCMOV.VLFRETE, 0)        VLFRETECIF     
-     , NVL(PCMOV.PERCIPI, 0)        PERCIPI        
-     , NVL(PCMOV.VLIPI, 0)          VLIPI          
-     , NVL(PCMOV.PERCOUTRASDESP, 0) PERCOUTRASDESP 
-     , NVL(PCMOV.VLOUTRASDESP, 0)   VLOUTRASDESP   
-     , NVL(PCMOV.PERCST, 0)         PERCST         
-     , NVL(PCMOV.ST,0)              VLST           
-     , NVL(PCNFENT.VLTOTAL, 0)      VLTOTAL        
-     , PCNFENT.CODFORNEC                           
-     , PCFORNEC.FORNECEDOR                         
-     , PCPRODUT.CODEPTO                            
-     , PCPRODUT.CODSEC                             
-     , PCMOV.CODOPER                               
-     , PCPRODUT.RUA                                
-     , PCPRODUT.NUMERO                             
-     , PCPRODUT.APTO                               
-     , PCDEPTO.DESCRICAO NOMEDEPTO                 
-     , PCMOV.NUMNOTA                               
-     , PCMOV.NUMPED                                
-     , PCNFENT.CODFUNCLANC                         
-     , PCEMPR.NOME NOMEFUNCIONARIO                 
-     , PCPRODUT.CODAUXILIAR                        
-     , PCPRODUT.CODAUXILIAR2                       
-     , PCPRODUT.CODFAB                             
-     , PCNFENT.CODFILIAL                           
-     , PCFILIAL.RAZAOSOCIAL AS FILIAL              
-     , PCPRODUT.EMBALAGEM                                                                                                                                          
-     , DECODE(NVL(PCMOV.PUNIT, 0), 0, NVL(PCMOV.PUNITCONT, 0), PCMOV.PUNIT) PUNIT                                                                                  
-     , PCMOV.CUSTOFINANT                                                                                                                                           
-     , PCMOV.CUSTOFIN                                                                                                                                              
-     , ( DECODE(PCMOV.STATUS, 'A', NVL(PCMOV.QTCONT,0), NVL(PCMOV.QT,0)) )                                                                          QTENTRAD     
-     , (( DECODE(PCMOV.STATUS, 'A', NVL(PCMOV.QTCONT,0), NVL(PCMOV.QT,0))) * DECODE(NVL(PCMOV.PUNIT, 0), 0, NVL(PCMOV.PUNITCONT, 0), PCMOV.PUNIT) )   VLENTRADB     
-     , (( DECODE(PCMOV.STATUS, 'A', NVL(PCMOV.QTCONT,0), NVL(PCMOV.QT,0))) * Nvl(PCMOV.CUSTOFIN,0))                                                   VLCUSTOFIN    
-     , (( DECODE(PCMOV.STATUS, 'A', NVL(PCMOV.QTCONT,0), NVL(PCMOV.QT,0))) * DECODE(NVL(PCMOV.PUNIT, 0), 0, NVL(PCMOV.PUNITCONT, 0), PCMOV.PUNIT) )   VLENTRAD      
-     , (NVL (PCPRODUT.PESOBRUTO,0) * ( DECODE(PCMOV.STATUS, 'A', NVL(PCMOV.QTCONT,0), NVL(PCMOV.QT,0))) )                                             TOTPESO       
-     , (NVL(PCPRODUT.VOLUME,0) * ( DECODE(PCMOV.STATUS, 'A', NVL(PCMOV.QTCONT,0), NVL(PCMOV.QT,0))) )                                                 TOTVOLUME     
-     , PCMOV.NUMLOTE                                                                                                                                                  
-     , PCMOV.DATAVALIDADE                                                                                                                                             
-     , PCMOV.NUMLOTEFAB                                                                                                                                               
-     , PCBONUSC.DTMONTAGEM                                                                                                                                            
-     , PCBONUSC.DTFECHAMENTOTOTAL                                                                                                                                     
-     ,CASE                                                                                                                                                            
-       WHEN((PCFILIAL.CONTROLENFEPORSERIE = 'S') AND(NVL(PCNFENT.CONSUMIUNUMNFE, 'N') = 'N'))                                                                   
-          THEN 'S'                                                                                                                                                  
-       ELSE 'N'                                                                                                                                                     
-      END NFPROVISORIO                                                                                                                                                
-  FROM PCMOV                                                                                                                                                          
-     , PCPRODUT                                                                                                                                                       
-     , PCFORNEC                                                                                                                                                       
-     , PCDEPTO                                                                                                                                                        
-     , PCSECAO                                                                                                                                                        
-     , PCNFENT                                                                                                                                                        
-     , PCEMPR                                                                                                                                                         
-     , PCCONSUM                                                                                                                                                       
-     , PCPRODFILIAL                                                                                                                                                   
-     , PCPEDIDO                                                                                                                                                       
-     , PCFILIAL                                                                                                                                                       
-     , PCBONUSC                                                                                                                                                       
- WHERE (PCEMPR.MATRICULA(+) = PCNFENT.CODFUNCLANC                                                                                                                      
-   AND (PCMOV.CODPROD = PCPRODUT.CODPROD)                                                                                                                             
-   AND PCMOV.NUMTRANSENT  = PCNFENT.NUMTRANSENT                                                                                                                       
-   AND PCNFENT.CODFILIAL = PCFILIAL.CODIGO                                                                                                                            
-   AND PCMOV.CODOPER NOT IN ('ED','EA')                                                                                                                           
-   AND PCMOV.DTCANCEL IS NULL                                                                                                                                         
-   AND PCPRODUT.CODEPTO = PCDEPTO.CODEPTO                                                                                                                             
-   AND PCPRODUT.CODSEC    = PCSECAO.CODSEC(+)                                                                                                                         
-   AND PCMOV.CODPROD = PCPRODFILIAL.CODPROD                                                                                                                           
-   AND PCMOV.CODFILIAL = PCPRODFILIAL.CODFILIAL                                                                                                                       
-   AND PCMOV.NUMPED = PCPEDIDO.NUMPED(+)                                                                                                                              
-   AND (PCMOV.QT > 0 OR (PCMOV.QTCONT > 0 AND PCNFENT.TIPODESCARGA = '4'))                                                                                          
-   AND PCNFENT.CODFORNEC = PCFORNEC.CODFORNEC                                                                                                                         
-   AND PCNFENT.TIPODESCARGA NOT IN ('6', '7', '8', 'N', 'F') 
-   AND PCNFENT.NUMBONUS    = PCBONUSC.NUMBONUS(+) 
-  AND ((PCNFENT.CODCONT = NVL(PCNFENT.CODCONTFOR,PCCONSUM.CODCONTFOR)) OR 
-  (PCNFENT.CODCONT = PCCONSUM.CODCONTAJUSTEEST AND PCNFENT.TIPODESCARGA = '4') OR 
-  (PCNFENT.TIPODESCARGA = 'S' AND ((PCNFENT.ESPECIE = 'NF') OR (PCNFENT.ESPECIE = 'NE'))))) AND (PCPRODUT.CODEPTO IN ( 100 )) 
-  AND PCMOV.DTMOV BETWEEN TO_DATE('01/'||TO_CHAR(ADD_MONTHS (TRUNC(SYSDATE),-0),'MM/YYYY'), 'DD/MM/YYYY')
-                     AND LAST_DAY(ADD_MONTHS(TRUNC(SYSDATE),-0)) /*PERIODO MÊS ATUAL*/
- AND PCNFENT.CODFILIAL IN ('1') -- FILIAL DEFINIDA
- AND PCMOV.CODOPER LIKE 'E%'
-ORDER BY PCPRODUT.CODEPTO, PCFORNEC.FORNECEDOR, PCMOV.NUMNOTA, PCMOV.DTMOV, PCPRODUT.DESCRICAO
-) TABELA
+
+SELECT   SUM (tabela.VLTOTAL) vltotal FROM   (
+SELECT   SUM(compra.vlentrad) VLENTRAD,
+                   saida.vldevol,
+                   SUM(compra.vlentrad) - saida.vldevol vltotal
+            FROM   (  SELECT   ( (DECODE (pcmov.status,
+                                          'A', NVL (pcmov.qtcont, 0),
+                                          NVL (pcmov.qt, 0)))
+                                * DECODE (NVL (pcmov.punit, 0),
+                                          0, NVL (pcmov.punitcont, 0),
+                                          pcmov.punit))
+                                   vlentrad
+                        FROM   pcmov,
+                               pcprodut,
+                               pcfornec,
+                               pcdepto,
+                               pcsecao,
+                               pcnfent,
+                               pcempr,
+                               pcconsum,
+                               pcprodfilial,
+                               pcpedido,
+                               pcfilial,
+                               pcbonusc
+                       WHERE   (    pcempr.matricula(+) = pcnfent.codfunclanc
+                                AND (pcmov.codprod = pcprodut.codprod)
+                                AND pcmov.numtransent = pcnfent.numtransent
+                                AND pcnfent.codfilial = pcfilial.codigo
+                                AND pcmov.codoper NOT IN ('ED', 'EA')
+                                AND pcmov.dtcancel IS NULL
+                                AND pcprodut.codepto = pcdepto.codepto
+                                AND pcprodut.codsec = pcsecao.codsec(+)
+                                AND pcmov.codprod = pcprodfilial.codprod
+                                AND pcmov.codfilial = pcprodfilial.codfilial
+                                AND pcmov.numped = pcpedido.numped(+)
+                                AND (pcmov.qt > 0
+                                     OR (pcmov.qtcont > 0
+                                         AND pcnfent.tipodescarga = '4'))
+                                AND pcnfent.codfornec = pcfornec.codfornec
+                                AND pcnfent.tipodescarga NOT IN
+                                           ('6', '7', '8', 'N', 'F')
+                                AND pcnfent.numbonus = pcbonusc.numbonus(+)
+                                AND ( (pcnfent.codcont =
+                                           NVL (pcnfent.codcontfor,
+                                                pcconsum.codcontfor))
+                                     OR (pcnfent.codcont =
+                                             pcconsum.codcontajusteest
+                                         AND pcnfent.tipodescarga = '4')
+                                     OR (pcnfent.tipodescarga = 'S'
+                                         AND ( (pcnfent.especie = 'NF')
+                                              OR (pcnfent.especie = 'NE')))))
+                               AND (pcprodut.codepto IN (100))
+                               AND pcmov.dtmov BETWEEN TO_DATE (
+                                                           '01/'
+                                                           || TO_CHAR (
+                                                                  ADD_MONTHS (
+                                                                      TRUNC(SYSDATE),
+                                                                      -0),
+                                                                  'MM/YYYY'),
+                                                           'DD/MM/YYYY')
+                                                   AND  LAST_DAY(ADD_MONTHS (
+                                                                     TRUNC(SYSDATE),
+                                                                     -0)) /*PERIODO MÊS ATUAL*/
+                               AND pcnfent.codfilial IN ('1') -- FILIAL DEFINIDA
+                               AND pcprodut.codepto IN (100) --PARAMETRO CLIENTE
+                               AND pcprodut.codsec IN (101) --PARAMETRO CLIENTE
+                               AND pcmov.codoper LIKE 'E%'
+                                ) compra,
+                   (  SELECT                                            
+                               SUM (NVL (pcprest.valor, 0)) vldevol
+                        FROM   pcnfsaid, pcprest, pcclient
+                       WHERE   pcnfsaid.numtransvenda = pcprest.numtransvenda
+                               AND pcprest.codcli = pcclient.codcli
+                               AND pcprest.dtpag IS NULL
+                               AND pcnfsaid.dtsaida BETWEEN TO_DATE (
+                                                                '01/'
+                                                                || TO_CHAR (
+                                                                       ADD_MONTHS (
+                                                                           TRUNC(SYSDATE),
+                                                                           -0),
+                                                                       'MM/YYYY'),
+                                                                'DD/MM/YYYY')
+                                                        AND  LAST_DAY(ADD_MONTHS (
+                                                                          TRUNC(SYSDATE),
+                                                                          -0))
+                               AND pcnfsaid.dtcancel IS NULL
+                               AND pcnfsaid.codfilial = '1'
+                               AND pcnfsaid.codfiscal IN
+                                          (532, 632, 732, 5202, 6202)
+                    ORDER BY   vldevol DESC) saida
+                    GROUP BY saida.vldevol
+                    ) tabela;
